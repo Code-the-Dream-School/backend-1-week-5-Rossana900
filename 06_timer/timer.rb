34f814109caab3bffbba96fff201181f57ce00d7
Rossana Piña <rossana.godfrey@gmail.com>
2020-09-09 20:51:19 -0400

    finished all the ruby programs

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,11 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end  
+  def time_string
+    "#{format('%02d', @seconds/3600)}:#{format('%02d', @seconds/60%60)}:#{format('%02d', @seconds%60)}"  
+
+  end  
 end
